1b2638ac82ab53193574d3eed70a4e1e6f3f1d5f
Arpit Bhardwaj <bhardwajarpit79@gmail.com>
2026-06-25 11:14:49 +0530

    Auto-create admin user in build step

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth.models import User
+from masters.models import UserProfile
+if not User.objects.filter(username='admin').exists():
+    u = User.objects.create_superuser('admin', 'admin@ctcsolution.in', '1234')
+    UserProfile.objects.get_or_create(user=u, defaults={'role': 'admin'})
+    print('Admin created')
+else:
+    u = User.objects.get(username='admin')
+    u.set_password('1234')
+    u.save()
+    print('Admin password updated')
+"
